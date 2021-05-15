73c7431145cca9ba09ce151375c7ab0d3f126731
Rashad-Muntar <muntartoure@gmail.com>
2021-05-15 09:18:10 +0000

    resolve heroku error

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,5 @@ module Portfolio
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
-
-
   config.assets.initialize_on_precompile = false
 end
