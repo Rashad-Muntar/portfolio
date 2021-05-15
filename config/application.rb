2bb4cddee99f8e7d235aa1c33380f6dfbb2e560f
Rashad-Muntar <muntartoure@gmail.com>
2021-05-15 09:10:41 +0000

    resovle heroku error

--- config/application.rb
+++ config/application.rb
@@ -19,4 +19,7 @@ module Portfolio
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
+
+
+  config.assets.initialize_on_precompile = false
 end
